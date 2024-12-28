4e6e9173e0c7b85c4e4c571a315d98d2c00327cf
nmeusling <15682036+nmeusling@users.noreply.github.com>
2024-12-28 10:20:11 -0500

    Add player and turn functionality

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
 
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     pygame.display.set_caption("Asteroids")
     _window_size = (SCREEN_WIDTH, SCREEN_HEIGHT)
     screen = pygame.display.set_mode(_window_size)
@@ -18,10 +22,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 is_running = False
-
-        screen.blit(background, (0,0))        
-    
+        screen.fill("black")
+        screen.blit(background, (0,0))  
+        player.update(dt)      
+        player.draw(screen)
         pygame.display.update()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
